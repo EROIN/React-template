d7cb6d3bc724cdbad0dfa5d45fc9ab00ee98a4d8
Asheesh <a@a.c>
2022-01-05 14:36:16 +0530

    test

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -7,10 +7,13 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
+  selectStatus,
 } from '../../redux/modules/counter.module';
 
 export function Home(props) {
   const count = useSelector(selectCount);
+  const status = useSelector(selectStatus);
+  const dispatch = useDispatch();
   // console.log(count);
 
   const goToHome2 = () => {
@@ -21,9 +24,12 @@ export function Home(props) {
   return (
     <div className="App">
       Home
-      <button  onClick={goToHome2}/>
+      <button  onClick={goToHome2}>asass</button>
+      <button  onClick={() => dispatch(decrement())}>dec</button>
+      <button  onClick={() => dispatch(incrementAsync(10))}>increment</button>
       <div>
         {count}
+        {status}
       </div>
     </div>
   );
