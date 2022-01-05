9d80d1e3c194fc1d447631592600caaebacd4852
Asheesh <a@a.c>
2022-01-05 12:54:56 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router'
+import { Route, Switch } from 'react-router'
+
 import './App.css';
+import { store, history } from './redux/store';
+
+import { Home } from './containers/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store()}>
+      <ConnectedRouter history={history}>
+        <>
+          <Switch>
+            <Route exact path="/" render={Home} />
+            <Route exact path="/test" render={() => (<><h1>test</h1></>)} />
+            <Route render={() => (<div>Miss</div>)} />
+          </Switch>
+        </>
+      </ConnectedRouter>
+    </Provider>
   );
 }
 
